3b8fe41f239516604ff5dcaa8773af1289713f50
johan gace <gace.johan3@gmail.com>
2021-04-27 11:02:20 -0400

    first commit

---
+++ frontend/pages/_app.js
@@ -0,0 +1,34 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import NProgress from 'nprogress';
+import Router from 'next/router';
+import { ApolloProvider } from '@apollo/client';
+import Page from '../components/Page';
+import '../components/styles/nprogress.css';
+import withData from '../lib/withData';
+// loading
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
+// apoll0 comes from withData below
+function MyApp({ Component, pageProps, apollo }) {
+  console.log(apollo);
+  return (
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
+  );
+}
+
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
+
+export default withData(MyApp);
---
+++ frontend/pages/orders.js
@@ -0,0 +1,6 @@
+export default function orderPage(){
+
+    return <div>
+        <p>hello</p>
+    </div>
+}
